b51ce9baa3e72b3ecdb6094c3b99dcab7a9d97c8
AlenaVP <poprugo@gmail.com>
2023-05-09 17:49:51 +0300

    feat: add a solution of 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const fsPromises = fs.promises;
+
+const sourseDirPath = path.join(__dirname, 'styles');
+const destPath = path.join(__dirname, 'project-dist', 'bundle.css');
+const ouput = fs.createWriteStream(destPath);
+
+fsPromises
+  .readdir(sourseDirPath, { withFileTypes: true })
+  .then((files) => {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const fileExt = path.parse(file.name).ext;
+        if (fileExt === '.css') {
+          const input = fs.createReadStream(path.join(sourseDirPath, file.name), 'utf-8');
+          input.on('data', chunk => ouput.write(chunk + '\n'));
+        }
+      }
+    });
+  });
